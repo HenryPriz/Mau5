9a3279288f49d991d1a5e53e2bef46bbab9ab674
Henry Price <xeniumh@gmail.com>
2019-02-19 17:27:50 +0000

    Spring Clean
    Needing to clean up the code, lets start!

---
+++ cleanMain.js
@@ -0,0 +1,26 @@
+'use strict'
+
+// List of rotator sensitivities for games
+var battlefield_v = 0.00598718344569206;
+var source_engine = 0.022;
+var overwatch = 0.0066;
+var rainbow_six_siege = (0.18/Math.PI)/10;
+var battlefield_3 = 0.52941;
+var borderlands_2 = 0.0055;
+var duke_nukem_forever = 1.714;
+var fornite = 0.5715;
+var planetside_2 = 0.36;
+var pubg = 2.22222;
+var unreal_tournament = 0.0596;
+
+
+
+// Main converter between games, used to calculate new hipfire sensitivity
+function hipConv() {
+    
+}
+
+// Secondary converter between games, used to calculate new ADS sensitivity if applicable
+function adsConv() {
+
+}
