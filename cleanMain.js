aa99e5c28d7cc7350639b719a0c848a0472e253b
Henry Price <xeniumh@gmail.com>
2019-02-19 22:21:55 +0000

    Array

--- cleanMain.js
+++ cleanMain.js
@@ -1,23 +1,24 @@
 'use strict'
 
 // List of rotator sensitivities for games
-var battlefield_v = 0.00598718344569206;
-var source_engine = 0.022;
-var overwatch = 0.0066;
-var rainbow_six_siege = (0.18/Math.PI)/10;
-var battlefield_3 = 0.52941;
-var borderlands_2 = 0.0055;
-var duke_nukem_forever = 1.714;
-var fornite = 0.5715;
-var planetside_2 = 0.36;
-var pubg = 2.22222;
-var unreal_tournament = 0.0596;
+var BFV = 0.00598718344569206;
+var SOURCE = 0.022;
+var OW = 0.0066;
+var R6S = (0.18/Math.PI)/10;
+var BF3 = 0.52941;
+var BL2 = 0.0055;
+var DNF = 1.714;
+var FN = 0.5715;
+var PS2 = 0.36;
+var PUBG = 2.22222;
+var UT = 0.0596;
 
 
 
 // Main converter between games, used to calculate new hipfire sensitivity
 function hipConv() {
-    
+    var hipSens;
+    var sensIncr;
 }
 
 // Secondary converter between games, used to calculate new ADS sensitivity if applicable
