10c20ee6098d3c9b7fa2111caa8cf425c6d865f6
Henry Price <xeniumh@gmail.com>
2019-05-14 22:59:55 +0100

    2.0 WIP
    > Changed colour scheme
    > Changed layout
    > Cleaned up JS
    - > Introduced an array
    - > Added more modularity

--- cleanMain.js
+++ cleanMain.js
@@ -1,27 +1,59 @@
 'use strict'
 
 // List of rotator sensitivities for games
-var BFV = 0.00598718344569206;
-var SOURCE = 0.022;
-var OW = 0.0066;
-var R6S = (0.18/Math.PI)/10;
-var BF3 = 0.52941;
-var BL2 = 0.0055;
-var DNF = 1.714;
-var FN = 0.5715;
-var PS2 = 0.36;
-var PUBG = 2.22222;
-var UT = 0.0596;
+var rotators = ["0.00598718344569206", "0.022", "0.0066", (0.18/Math.PI)/10, "0.52941", "0.0055", "1.714", "0.5715", "0.36", "2.22222", "0.0596", (0.18/Math.PI)/10];
 
+// Global Variables
 
+function conv() {
+
+    var userOrigSens = document.getElementById("inpGameSens").value;
+    var userOrigGameVal = document.getElementById("lInputGame").value;
+    var userDesGameVal = document.getElementById("rInputGame").value;
+    hipConv(userOrigSens, userOrigGameVal, userDesGameVal);
+}
+
+function advInp() {
+    var user360 = document.getElementById("inpCM360").value;
+    var userOrigGameVal = document.getElementById("lInputGame").value;
+    var userDesGameVal = document.getElementById("rInputGame").value;
+    var userDPI = document.getElementById("inpDPI").value;
+    var userSensIncr = (360 / ( ( user360 / 2.54) * userDPI ));
+    var userOrigSens = (userSensIncr / rotators[userOrigGameVal]);
+
+    hipConv(userOrigSens, userOrigGameVal, userDesGameVal);
+}
 
 // Main converter between games, used to calculate new hipfire sensitivity
-function hipConv() {
-    var hipSens;
-    var sensIncr;
+function hipConv(userOrigSens, userOrigGameVal, userDesGameVal)  {
+
+    var userOrigGameRotat = rotators[userOrigGameVal];
+
+    var userDesGameRotat = rotators[userDesGameVal];
+
+    var userSensIncr = (userOrigSens * userOrigGameRotat);
+    var newHipSens = (userSensIncr / userDesGameRotat);
+
+    document.getElementById("outGameSens").value = newHipSens;
 }
 
 // Secondary converter between games, used to calculate new ADS sensitivity if applicable
 function adsConv() {
 
+}
+// Shows and hides the advanced input
+function advInpSel() {
+    if (document.getElementById("leftAdv").checked) {
+        document.getElementById("inpGameSens").disabled = true;
+        document.getElementById("labelDPI").style.display = null;
+        document.getElementById("inpDPI").style.display = null;
+        document.getElementById("labelInp360").style.display = null;
+        document.getElementById("inpCM360").style.display = null;
+    } else if (!document.getElementById("leftAdv").checked) {
+        document.getElementById("inpGameSens").disabled = false;
+        document.getElementById("labelDPI").style.display = 'none';
+        document.getElementById("inpDPI").style.display = 'none';
+        document.getElementById("labelInp360").style.display = 'none';
+        document.getElementById("inpCM360").style.display = 'none';
+    }
 }
